c6598d08330084ce88c706b5263965d5f23b53a6
Test <itsaryaman23@gmail.com>
2024-12-12 13:37:39 +0530

    Pushing code for submission app incomplete

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Hstyles from "./Header.module.css";
+import logo from "../../assets/logo.svg";
+import Container from "react-bootstrap/Container";
+
+const Header = () => {
+  return (
+    <>
+      <div className={Hstyles.topBanner}>
+        <p className={Hstyles.bannerText}>
+          {" "}
+          The health and well-being of our patients and their health care team
+          will always be our priority, so we follow the best practices for
+          cleanliness.
+        </p>
+      </div>
+
+
+      <div className={Hstyles.hero}>
+        {/* <Container> */}
+          <nav className={Hstyles.nav}>
+            <div className={Hstyles.navAlign}>
+              <img src={logo} alt="logo" width="92px" heigth="27px" />
+              <div className={Hstyles.navBtnBox}>
+                <p>Find Doctors</p>
+                <p>Hospitals</p>
+                <p>Medicines</p>
+                <p>Surgeries</p>
+                <p>Software for Providers</p>
+                <p>Facilities</p>
+                <button className={Hstyles.bookingBtn}>My Bookings</button>
+              </div>
+            </div>
+          </nav>
+        {/* </Container> */}
+      </div>
+    </>
+  );
+};
+
+export default Header;
